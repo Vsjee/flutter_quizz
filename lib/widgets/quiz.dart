941d4118af5860584d1b0325479d70b31325ab45
david hernandez <davidfelipehernandez1@gmail.com>
2023-09-08 22:25:26 -0500

    add quiz widget

---
+++ lib/widgets/quiz.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+import 'package:quizz_app/widgets/questions_screen.dart';
+import 'package:quizz_app/widgets/home_entry.dart';
+
+const List<Color> colors = [
+  Color.fromARGB(255, 1, 30, 108),
+  Color.fromARGB(255, 14, 40, 112),
+];
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  Widget? activeScreen;
+
+  @override
+  void initState() {
+    super.initState();
+    activeScreen = Home(switchScreen);
+  }
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = const QuestionScreen();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          alignment: Alignment.center,
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                colors: colors,
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight),
+          ),
+          child: activeScreen,
+        ),
+      ),
+    );
+  }
+}
