76ba3a826c9def2c43db7fea62b064bd954ce465
david hernandez <davidfelipehernandez1@gmail.com>
2023-09-08 22:25:05 -0500

    refactor main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,7 @@
 import 'package:flutter/material.dart';
 
-import 'package:quizz_app/widgets/home_entry.dart';
+import 'package:quizz_app/widgets/quiz.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-const List<Color> colors = [
-  Color.fromARGB(255, 1, 30, 108),
-  Color.fromARGB(255, 14, 40, 112),
-];
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        useMaterial3: true,
-      ),
-      home: const Scaffold(
-        body: Center(child: Home(colors: colors)),
-      ),
-    );
-  }
+  runApp(const Quiz());
 }
