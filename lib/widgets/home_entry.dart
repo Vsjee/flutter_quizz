91144f146cd0d27a668f0d61ac02d3779e31309f
david hernandez <davidfelipehernandez1@gmail.com>
2023-09-08 22:25:50 -0500

    update home widget

--- lib/widgets/home_entry.dart
+++ lib/widgets/home_entry.dart
@@ -1,53 +1,37 @@
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
-  const Home({super.key, required this.colors});
+  const Home(this.startQuiz, {super.key});
 
-  final List<Color> colors;
+  final void Function() startQuiz;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-            colors: colors,
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight),
-      ),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Image.asset(
-            'assets/images/quiz-logo.png',
-            width: 300,
-            color: const Color.fromARGB(234, 255, 60, 0),
-          ),
-          // Opacity(
-          //   opacity: 0.4,
-          //   child: Image.asset(
-          //     'assets/images/quiz-logo.png',
-          //     width: 300,
-          //   ),
-          // ),
-          const SizedBox(
-            height: 40,
-          ),
-          const Text(
-            'Formula 1 Quizz',
-            style: TextStyle(fontSize: 20, color: Colors.white),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          OutlinedButton.icon(
-            onPressed: () {},
-            style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
-            label: const Text('Start'),
-            icon: const Icon(Icons.arrow_forward),
-          )
-        ],
-      ),
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(
+          'assets/images/quiz-logo.png',
+          width: 300,
+          color: const Color.fromARGB(234, 255, 60, 0),
+        ),
+        const SizedBox(
+          height: 40,
+        ),
+        const Text(
+          'Formula 1 Quizz',
+          style: TextStyle(fontSize: 20, color: Colors.white),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        OutlinedButton.icon(
+          onPressed: startQuiz,
+          style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+          label: const Text('Start'),
+          icon: const Icon(Icons.arrow_forward),
+        )
+      ],
     );
   }
 }
