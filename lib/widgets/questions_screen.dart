377da87763b2d0781c0cb83ee31a42215b6c4935
david hernandez <davidfelipehernandez1@gmail.com>
2023-09-10 18:40:29 -0500

    add basic answers

--- lib/widgets/questions_screen.dart
+++ lib/widgets/questions_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'package:quizz_app/data/questions.dart';
+
+import '../ui/answer_button.dart';
+
 class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
 
@@ -10,8 +14,38 @@ class QuestionScreen extends StatefulWidget {
 }
 
 class _QuestionScreenState extends State<QuestionScreen> {
+  var currQuestionIdx = 0;
+
+  void answerQuestion() {
+    setState(() {
+      currQuestionIdx++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Text('text i am a custom wiget');
+    final currQuestion = questions[0];
+
+    return Padding(
+      padding: const EdgeInsets.only(left: 35, right: 35),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(
+            currQuestionIdx.toString(),
+            style: const TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 18,
+            ),
+          ),
+          const SizedBox(height: 30),
+          ...currQuestion.getSuffledAnswers().map((item) {
+            return AnswerButton(item, answerQuestion);
+          }),
+        ],
+      ),
+    );
   }
 }
