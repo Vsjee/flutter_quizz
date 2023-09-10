9dbd1a0ec361c499a8cb7c0089558383c6a3de2c
david hernandez <davidfelipehernandez1@gmail.com>
2023-09-10 18:20:43 -0500

    add custom button widget

---
+++ lib/ui/answer_button.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class AnswerButton extends StatelessWidget {
+  const AnswerButton(this.text, this.onTap, {super.key});
+
+  final String text;
+  final void Function() onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {},
+      style: ElevatedButton.styleFrom(
+          backgroundColor: const Color.fromARGB(149, 42, 138, 255),
+          foregroundColor: Colors.white,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+          padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 28)),
+      child: Text(text),
+    );
+  }
+}
