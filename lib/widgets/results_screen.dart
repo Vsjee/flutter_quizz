b442072a2de1e2a3db08ccfb7b03142f0527eb66
david hernandez <davidfelipehernandez1@gmail.com>
2023-09-20 00:43:25 -0500

    add getSummary method

--- lib/widgets/results_screen.dart
+++ lib/widgets/results_screen.dart
@@ -1,7 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:quizz_app/data/questions.dart';
+import 'package:quizz_app/widgets/questions_summary.dart';
 
 class ResultsScreen extends StatelessWidget {
-  const ResultsScreen({super.key});
+  const ResultsScreen({super.key, required this.chosenAnswers});
+
+  final List<String> chosenAnswers;
+
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+
+    for (var i = 0; i < chosenAnswers.length; i++) {
+      summary.add(
+        {
+          'questions_idx': i,
+          'question': questions[i].questions,
+          'correct_answer': questions[i].answers[0],
+          'user_answer': chosenAnswers[i]
+        },
+      );
+    }
+
+    return summary;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +33,9 @@ class ResultsScreen extends StatelessWidget {
         const SizedBox(
           height: 20,
         ),
-        const Text('List of answers and questions'),
+        QuestionsSummary(
+          summaryData: getSummaryData(),
+        ),
         const SizedBox(
           height: 20,
         ),
