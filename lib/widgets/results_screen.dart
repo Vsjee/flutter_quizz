82e7d57e6ffbc79de42c18460a07d04aa450bc76
David Hernandez <davidfelipehernandez1@gmail.com>
2023-10-08 17:42:43 -0500

    refactor results screen

--- lib/widgets/results_screen.dart
+++ lib/widgets/results_screen.dart
@@ -3,9 +3,11 @@ import 'package:quizz_app/data/questions.dart';
 import 'package:quizz_app/widgets/questions_summary.dart';
 
 class ResultsScreen extends StatelessWidget {
-  const ResultsScreen({super.key, required this.chosenAnswers});
+  const ResultsScreen(
+      {super.key, required this.chosenAnswers, required this.restartQuiz});
 
   final List<String> chosenAnswers;
+  final void Function() restartQuiz;
 
   List<Map<String, Object>> getSummaryData() {
     final List<Map<String, Object>> summary = [];
@@ -26,20 +28,32 @@ class ResultsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final summaryData = getSummaryData();
+    final numberOfTotalQuestions = questions.length;
+    final numberCorrectQuestions = summaryData.where((data) {
+      return data['correct_answer'] == data['user_answer'];
+    }).toList();
+
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
-        const Text('Answers'),
-        const SizedBox(
-          height: 20,
-        ),
-        QuestionsSummary(
-          summaryData: getSummaryData(),
+        Text(
+          'You answered ${numberCorrectQuestions.length} out of $numberOfTotalQuestions questions correctly!',
+          style: const TextStyle(color: Colors.white, fontSize: 22),
+          textAlign: TextAlign.center,
         ),
         const SizedBox(
           height: 20,
         ),
-        TextButton(onPressed: () {}, child: const Text('Restar F1 Quizz'))
+        QuestionsSummary(summaryData),
+        const SizedBox(
+          height: 20,
+        ),
+        FilledButton.icon(
+          onPressed: restartQuiz,
+          label: const Text('Restar F1 Quizz'),
+          icon: const Icon(Icons.restart_alt_rounded),
+        )
       ],
     );
   }
