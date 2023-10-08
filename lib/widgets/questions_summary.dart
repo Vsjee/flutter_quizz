32d5d8c4e2ed909135573b6d9a340583d6709cbb
David Hernandez <davidfelipehernandez1@gmail.com>
2023-10-08 17:42:19 -0500

    refactor and use question widget

--- lib/widgets/questions_summary.dart
+++ lib/widgets/questions_summary.dart
@@ -1,31 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:quizz_app/ui/result_item.dart';
 
 class QuestionsSummary extends StatelessWidget {
-  const QuestionsSummary({super.key, required this.summaryData});
+  const QuestionsSummary(this.summaryData, {super.key});
 
   final List<Map<String, Object>> summaryData;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding: const EdgeInsets.only(left: 35, right: 35),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: summaryData.map((data) {
-            return Row(children: [
-              Text(((data['questions_idx'] as int) + 1).toString()),
-              Expanded(
-                child: Column(
-                  children: [
-                    Text(data['question'] as String),
-                    const SizedBox(height: 6),
-                    Text(data['user_answer'] as String),
-                    Text(data['correct_answer'] as String),
-                  ],
-                ),
-              )
-            ]);
-          }).toList(),
+        padding: const EdgeInsets.only(left: 30, right: 30),
+        child: SizedBox(
+          height: 480,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: summaryData.map((data) {
+                if (data['correct_answer'] == data['user_answer']) {
+                  return ResultItem(data, Colors.greenAccent);
+                } else {
+                  return ResultItem(data, Colors.redAccent);
+                }
+              }).toList(),
+            ),
+          ),
         ));
   }
 }
