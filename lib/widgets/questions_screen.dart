2cb98372b73991129662605332211d0225c7be08
David Hernandez <davidfelipehernandez1@gmail.com>
2023-10-20 19:18:53 -0500

    add api call and data setup

--- lib/widgets/questions_screen.dart
+++ lib/widgets/questions_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:quizz_app/core/models/questions_model.dart';
+import 'package:quizz_app/core/services/questions_service.dart';
 
 import 'package:quizz_app/data/questions.dart';
 
@@ -18,6 +20,26 @@ class QuestionScreen extends StatefulWidget {
 
 class _QuestionScreenState extends State<QuestionScreen> {
   var currQuestionIdx = 0;
+  QuestionsResponse? questionsResponse;
+  var isloaded = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // fetch data from API
+    getData();
+  }
+
+  getData() async {
+    questionsResponse = await QuestionsService().getData();
+
+    if (questionsResponse != null) {
+      setState(() {
+        isloaded = true;
+      });
+    }
+  }
 
   void answerQuestion(String selectedAnswer) {
     widget.onSelectAnswer(selectedAnswer);
@@ -31,28 +53,31 @@ class _QuestionScreenState extends State<QuestionScreen> {
   Widget build(BuildContext context) {
     final currQuestion = questions[currQuestionIdx];
 
-    return Padding(
-      padding: const EdgeInsets.only(left: 35, right: 35),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Text(
-            currQuestion.questions,
-            style: GoogleFonts.roboto(
-              color: const Color.fromARGB(255, 202, 210, 251),
-              fontWeight: FontWeight.bold,
-              fontSize: 18,
-            ),
+    return Visibility(
+        visible: isloaded,
+        replacement: const Center(child: CircularProgressIndicator()),
+        child: Padding(
+          padding: const EdgeInsets.only(left: 35, right: 35),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                currQuestion.questions,
+                style: GoogleFonts.roboto(
+                  color: const Color.fromARGB(255, 202, 210, 251),
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+              const SizedBox(height: 30),
+              ...currQuestion.getSuffledAnswers().map((item) {
+                return AnswerButton(item, () {
+                  answerQuestion(item);
+                });
+              }),
+            ],
           ),
-          const SizedBox(height: 30),
-          ...currQuestion.getSuffledAnswers().map((item) {
-            return AnswerButton(item, () {
-              answerQuestion(item);
-            });
-          }),
-        ],
-      ),
-    );
+        ));
   }
 }
